614e97650b6d4a98ee0864f28319c7fb5ac61ca4
Chanhuyk Lee <cksgur0612@dgsw.hs.kr>
2023-05-23 17:19:59 +0900

    api gate way

---
+++ msa_service/src/test/java/com/example/msa_service/MsaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msa_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
