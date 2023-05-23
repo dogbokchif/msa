5f29d4599a26a75037be5339b5b4561ee2f8a2e4
Chanhuyk Lee <cksgur0612@dgsw.hs.kr>
2023-05-23 14:19:22 +0900

    readme

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# msa
+# 싱글벙글 msa
 재미진 msa 구현 연습
+<img src="https://cdn.inflearn.com/public/courses/328412/cover/4b378968-fcc9-47e7-b08d-a1635551b602/328412-eng-new.png">
--- src/main/java/com/msa/msa_work/MsaWorkApplication.java
+++ src/main/java/com/msa/msa_work/MsaWorkApplication.java
@@ -2,12 +2,12 @@ package com.msa.msa_work;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class MsaWorkApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MsaWorkApplication.class, args);
     }
-
 }
