b11aa845a7611a3958e8ba8cd56f84ebef7149c1
Chanhuyk Lee <cksgur0612@dgsw.hs.kr>
2023-05-23 14:07:25 +0900

    init

---
+++ src/main/java/com/msa/msa_work/MsaWorkApplication.java
@@ -0,0 +1,13 @@
+package com.msa.msa_work;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsaWorkApplication.class, args);
+    }
+
+}
