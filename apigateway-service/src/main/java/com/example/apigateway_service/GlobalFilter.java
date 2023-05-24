2e5562fffd2ba344dd177174e2e2432fc6956e88
Chanhuyk Lee <cksgur0612@dgsw.hs.kr>
2023-05-24 22:16:14 +0900

    마이크로서비스 수준의 오류 해결

---
+++ apigateway-service/src/main/java/com/example/apigateway_service/GlobalFilter.java
@@ -0,0 +1,33 @@
+package com.example.apigateway_service;
+
+import lombok.Data;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Component
+public class GlobalFilter extends AbstractGatewayFilterFactory<GlobalFilter.Config> {
+    public GlobalFilter() {
+        super(Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return ((exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+            ServerHttpResponse response = exchange.getResponse();
+
+            System.out.println(request.getPath());
+
+            // Custom Post Filter
+            return chain.filter(exchange);
+        });
+    }
+
+    public static class Config {
+
+    }
+}
